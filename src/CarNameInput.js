9971fc3c77a31df2996ff26f608f6f497dba9aca
10012 <nas7062@naver.com>
2025-10-27 20:59:40 +0900

    fix : 클래스명 대소문자 변경

--- src/CarNameInput.js
+++ src/CarNameInput.js
@@ -1,9 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "./constant/messages.js";
-import { CarInputValid } from "./validate/carInputValid.js";
+import { carInputValid } from "./validate/carInputValid.js";
 export const carNameInput = async () => {
   const carInput = await Console.readLineAsync(MESSAGE.CAR_INPUT);
   const carList = carInput.split(",").map((item) => item.trim());
-  const validCarList = CarInputValid(carList);
+  const validCarList = carInputValid(carList);
   return validCarList;
 };
