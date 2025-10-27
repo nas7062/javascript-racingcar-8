cc8cc0a4ff544a0bc530b65b1ec625ac0aa47a83
10012 <nas7062@naver.com>
2025-10-27 20:20:55 +0900

    test(InputTest) : 입력 유호성 검사 및 테스트

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,6 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import App from "../src/App.js";
-import { CAR_VALIDATE } from "../src/constant/messages.js";
+import { CAR_VALIDATE, COUNT_VALIDATE } from "../src/constant/messages.js";
 const mockQuestions = (inputs) => {
   Console.readLineAsync = jest.fn();
   Console.readLineAsync.mockImplementation(() => {
@@ -39,50 +39,47 @@ describe("자동차 입력 및 유효성검사", () => {
     await expect(app.run()).rejects.toThrow(CAR_VALIDATE.NAME_DUPLICATE);
   });
 
-  //   test.each([
-  //     "pobi,", // 끝 빈
-  //     ",pobi", // 앞 빈
-  //     "pobi,,woni", // 중간 빈
-  //     "", // 빈 문자열
-  //     "   ", // 공백만
-  //   ])('자동차 이름이 비어 있으면 에러를 발생시킨다.: "%s"', async (input) => {
-  //     mockQuestions([input]);
-  //     const app = new App();
-  //     await expect(app.run()).rejects.toThrow(ERROR_MESSAGES.EMPTY_NAME);
-  //   });
-  // });
+  test.each(["pobi,", ",pobi", "pobi,,woni", "", " "])(
+    "자동차 이름이 비어 있으면 에러를 발생시킨다.",
+    async (input) => {
+      mockQuestions([input]);
+      const app = new App();
+      await expect(app.run()).rejects.toThrow(CAR_VALIDATE.NAME_ZERO);
+    }
+  );
+});
 
-  // describe("시도 횟수 입력 및 검증", () => {
-  //   test("양의 정수를 정상적으로 입력받는다.", async () => {
-  //     mockQuestions(["pobi,woni", "1"]);
-  //     const app = new App();
-  //     await expect(app.run()).resolves.toBeUndefined();
-  //   });
+describe("시도 횟수 입력 및 검증", () => {
+  test("양의 정수를 정상적으로 입력받는다.", async () => {
+    mockQuestions(["pobi,woni", "1"]);
+    const app = new App();
+    await expect(app.run()).resolves.toBeUndefined();
+  });
 
-  //   test("시도 횟수 입력 시 앞뒤 공백을 제거한다.", async () => {
-  //     mockQuestions(["pobi,woni", "  5  "]);
-  //     const app = new App();
-  //     await expect(app.run()).resolves.toBeUndefined();
-  //   });
+  test("시도 횟수 입력 시 앞뒤 공백을 제거한다.", async () => {
+    mockQuestions(["pobi,woni", "  5  "]);
+    const app = new App();
+    await expect(app.run()).resolves.toBeUndefined();
+  });
 
-  //   test("시도 횟수가 숫자가 아니면 에러를 발생시킨다.", async () => {
-  //     mockQuestions(["pobi,woni", "a1"]);
-  //     const app = new App();
-  //     await expect(app.run()).rejects.toThrow(ERROR_MESSAGES.NOT_A_NUMBER);
-  //   });
+  test("시도 횟수가 숫자가 아니면 에러를 발생시킨다.", async () => {
+    mockQuestions(["pobi,woni", "a1"]);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow(COUNT_VALIDATE.NAN_NUMBER);
+  });
 
-  //   test("시도 횟수가 정수가 아니면 에러를 발생시킨다.", async () => {
-  //     mockQuestions(["pobi,woni", "5.5"]);
-  //     const app = new App();
-  //     await expect(app.run()).rejects.toThrow(ERROR_MESSAGES.INVALID_ROUND);
-  //   });
+  test("시도 횟수가 정수가 아니면 에러를 발생시킨다.", async () => {
+    mockQuestions(["pobi,woni", "5.5"]);
+    const app = new App();
+    await expect(app.run()).rejects.toThrow(COUNT_VALIDATE.NOT_INTEGER);
+  });
 
-  //   test.each(["0", "-5"])(
-  //     '시도 횟수가 1 미만이면 에러를 발생시킨다.: "%s"',
-  //     async (round) => {
-  //       mockQuestions(["pobi,woni", round]);
-  //       const app = new App();
-  //       await expect(app.run()).rejects.toThrow(ERROR_MESSAGES.INVALID_ROUND);
-  //     }
-  //   );
+  test.each(["0", "-5"])(
+    '시도 횟수가 1 미만이면 에러를 발생시킨다.: "%s"',
+    async (round) => {
+      mockQuestions(["pobi,woni", round]);
+      const app = new App();
+      await expect(app.run()).rejects.toThrow(COUNT_VALIDATE.NOT_ZERO);
+    }
+  );
 });
